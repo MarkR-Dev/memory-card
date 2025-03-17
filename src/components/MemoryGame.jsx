e43ee61cd92b65e0dbb59e42b46e8395d484d2c3
Mark Rodgers <markrodgers.dev@gmail.com>
2025-03-17 17:23:18 +0000

    refactor: Remove redundant state

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -6,40 +6,20 @@ import Card from "./Card";
 import "../styles/memory-game.css";
 
 function MemoryGame() {
-  const TOTAL_POKEMON = 10;
   const [pokemonData, setPokemonData] = useState([...stockData]);
-  const [selectedIds, setSelectedIds] = useState([]); // store in array here or setState true/false on each card?
-  const [currentScore, setCurrentScore] = useState(0); // need this or calculate on render based on length of selected array?
+  const [selectedIds, setSelectedIds] = useState([]);
   const [bestScore, setBestScore] = useState(0);
-  //
-  const currentS = selectedIds.length;
+  const currentScore = selectedIds.length;
+  const TOTAL_POKEMON = 10;
 
   function handleGuess(event) {
     const targetId = event.target.closest("[data-poke-id").dataset.pokeId;
     const shuffledPokemonData = shuffleArray(pokemonData);
 
-    /*
-    // Guess has been repeated
     if (selectedIds.includes(targetId)) {
+      // Guess has been repeated
       if (currentScore > bestScore) {
         setBestScore(currentScore);
-      }
-      setCurrentScore(0);
-      setSelectedIds([]);
-    } else {
-      // Guess hasn't been repeated
-      setSelectedIds([...selectedIds, targetId]);
-      setCurrentScore((currentScore) => currentScore + 1);
-    }
-
-    // Shuffle after each guess
-    setPokemonData(shuffledPokemonData);
-    */
-
-    // Guess has been repeated
-    if (selectedIds.includes(targetId)) {
-      if (currentS > bestScore) {
-        setBestScore(currentS);
       }
 
       setSelectedIds([]);
@@ -55,10 +35,7 @@ function MemoryGame() {
   return (
     <main>
       <div>
-        {/* <h2>{"Current Score: " + currentScore}</h2>
-        <h2>{"Best Score: " + bestScore}</h2> */}
-
-        <h2>{"Current Score: " + currentS}</h2>
+        <h2>{"Current Score: " + currentScore}</h2>
         <h2>{"Best Score: " + bestScore}</h2>
       </div>
       {pokemonData.map((pokemon) => {
